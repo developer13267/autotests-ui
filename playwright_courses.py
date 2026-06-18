f1ef5e4d1aa5713f3d23a85658d39be5bebb5811
developer13267 <116674359+developer13267@users.noreply.github.com>
2026-06-18 15:30:47 +0300

    Update playwright_courses.py

--- playwright_courses.py
+++ playwright_courses.py
@@ -2,3 +2,30 @@ from playwright.sync_api import sync_playwright
 
 with sync_playwright() as plawright:
     browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+
+    #Заполняем поле почты
+    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    email_input.fill("user.name@gmail.com")
+
+    #Заполняем поле имя пользователя
+    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    username_input.fill('username')
+
+    #Заполняем поле пароль
+    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    password_input.fill('password')
+
+    #Нажметм на кнопку Registration
+    registration_link = page.get_by_test_id('registration-page-registration-button')
+    registration_link.click()
+    
+with sync_playwright() as plawright:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state='browser-state.json')
+    page = context.new_page()
+
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login')
